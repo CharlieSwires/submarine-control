cf2136415525cd3515964a57ca3f47325ca660ff
unknown <charlieswires@gmail.com>
2024-03-01 20:37:34 +0000

    10fps SD

--- libcam.sh
+++ libcam.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash
 
 # Function to clean up on exit
 cleanup() {
@@ -10,5 +10,5 @@ cleanup() {
 trap cleanup EXIT
 
 # Start libcamera-vid and pipe its output directly to the GStreamer pipeline
-libcamera-vid -t 0 --width 640 --height 480 --codec h264 --framerate 30 --inline --listen -o - --bitrate=8000000 awb-mode=tungsten preview=false | \
+libcamera-vid -t 0 --width 720 --height 576 --codec h264 --framerate 10 --inline --listen -o - --bitrate=1000000 awb-mode=tungsten preview=false | \
 gst-launch-1.0 fdsrc ! h264parse ! rtph264pay name=pay0 pt=96 ! udpsink host=192.168.137.205 port=8554
