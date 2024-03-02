14f99c8c4328df667c4fe554c50c09f3bc002f6d
unknown <charlieswires@gmail.com>
2024-03-02 05:44:39 +0000

    got it working using cvlc

--- src/main/java/implementation/ImageCapture.java
+++ src/main/java/implementation/ImageCapture.java
@@ -29,7 +29,7 @@ public class ImageCapture {
 			// Set the command to execute
 			switch (resolution) {
 			case HD:
-				process = Runtime.getRuntime().exec("pkill vlc");
+				process = Runtime.getRuntime().exec("pkill vlc&");
 				break;
 			case SMALL:
 				process = Runtime.getRuntime().exec("libcamera-vid -t 0 --width 640 --height 480 --codec yuv420 --framerate 10 --inline --listen -o - | cvlc -vvv stream:///dev/stdin --sout '#transcode{vcodec=h264,vb=800,scale=Auto,acodec=none}:rtp{sdp=rtsp://192.168.137.205:8554/}' --demux=rawvideo --rawvid-fps=10 --rawvid-width=640 --rawvid-height=480 --rawvid-chroma=I420&");
@@ -39,23 +39,25 @@ public class ImageCapture {
 				break;
 			default:
 				throw new RuntimeException("Not implemented:"+resolution.toString());
-				
+
 			}
 
 
 			// Wait for the command to finish
-			int exitVal = process.waitFor();
-			if (resolution == Resolution.PHOTO && exitVal == 0) {
-				log.debug("Image captured successfully");
+			if (resolution == Resolution.PHOTO) {
+				int exitVal = process.waitFor();
+				if(exitVal == 0) {
+					log.debug("Image captured successfully");
 
-				// Load the image from the file
-				File imageFile = new File(outputFileName);
-				BufferedImage image = ImageIO.read(imageFile);
+					// Load the image from the file
+					File imageFile = new File(outputFileName);
+					BufferedImage image = ImageIO.read(imageFile);
 
-				// Return the loaded image
-				return image;
-			} else {
-				log.error("Image capture failed");
+					// Return the loaded image
+					return image;
+				} else {
+					log.error("Image capture failed");
+				}
 			}
 		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
