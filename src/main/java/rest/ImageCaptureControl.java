07eff92752d120e258193e1921103f667979508c
unknown <charlieswires@gmail.com>
2024-03-02 08:55:41 +0000

    got it working using cvlc

--- src/main/java/rest/ImageCaptureControl.java
+++ src/main/java/rest/ImageCaptureControl.java
@@ -23,31 +23,31 @@ public class ImageCaptureControl {
     @Autowired
     private ImageCapture ic;
 
-    @GetMapping(path = "capture/false", produces = "application/json")
-    public ResponseEntity<Integer> getImage() {
-        ic.captureImage(Resolution.SMALL); // Assuming captureImage() returns a BufferedImage
-		return new ResponseEntity<Integer>(0, HttpStatus.OK);
-    }
-    @GetMapping(path = "capture/true", produces = "application/json")
-    public ResponseEntity<Integer> getImageFull() {
-        ic.captureImage(Resolution.HD); // Assuming captureImage() returns a BufferedImage
-		return new ResponseEntity<Integer>(0, HttpStatus.OK);
-
-    }    
-    @GetMapping(path = "capture/photo", produces = "image/jpeg")
-    public void getPhotoFull(HttpServletResponse response) {
-        try {
-            BufferedImage image = ic.captureImage(Resolution.PHOTO); // Assuming captureImage() returns a BufferedImage
-            if (image != null) {
-                response.setContentType("image/jpeg");
-                ImageIO.write(image, "JPEG", response.getOutputStream());
-                response.getOutputStream().close();
-            } else {
-                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        }
-    }
+//    @GetMapping(path = "capture/false", produces = "application/json")
+//    public ResponseEntity<Integer> getImage() {
+//        ic.captureImage(Resolution.SMALL); // Assuming captureImage() returns a BufferedImage
+//		return new ResponseEntity<Integer>(0, HttpStatus.OK);
+//    }
+//    @GetMapping(path = "capture/true", produces = "application/json")
+//    public ResponseEntity<Integer> getImageFull() {
+//        ic.captureImage(Resolution.HD); // Assuming captureImage() returns a BufferedImage
+//		return new ResponseEntity<Integer>(0, HttpStatus.OK);
+//
+//    }    
+//    @GetMapping(path = "capture/photo", produces = "image/jpeg")
+//    public void getPhotoFull(HttpServletResponse response) {
+//        try {
+//            BufferedImage image = ic.captureImage(Resolution.PHOTO); // Assuming captureImage() returns a BufferedImage
+//            if (image != null) {
+//                response.setContentType("image/jpeg");
+//                ImageIO.write(image, "JPEG", response.getOutputStream());
+//                response.getOutputStream().close();
+//            } else {
+//                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+//        }
+//    }
 }
