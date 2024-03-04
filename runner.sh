35f80a6c783371408c4e30510acc35064ac55451
unknown <charlieswires@gmail.com>
2024-03-04 16:50:05 +0000

    Motor controller implementation

--- runner.sh
+++ runner.sh
@@ -2,7 +2,8 @@
 #!/bin/bash
 echo "===================================================running=========================================="
 #export GST_DEBUG=3
-sudo pigpiod&
+sudo pkill pigpiod
+sudo pigpiod &
 ~/submarine-control/libcam.sh &
 java -jar ~/submarine-control/target/submarine-control.jar &
 
