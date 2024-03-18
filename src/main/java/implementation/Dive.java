010cfe2f4bfc48d6c6f24ba0492c7b3f2eebe58e
unknown <charlieswires@gmail.com>
2024-03-18 23:43:28 +0000

    potential fix

--- src/main/java/implementation/Dive.java
+++ src/main/java/implementation/Dive.java
@@ -50,7 +50,6 @@ public class Dive {
 			deviceAccl = i2CProvider.create(configAccl);
 
 			// Initialize accelerometer
-			deviceAccl.writeRegister(0x24, (byte) 0x80); //reboot no fifo
 			deviceAccl.writeRegister(0x24, (byte) 0x00); //normal no fifo
 			deviceAccl.writeRegister(0x21, (byte) 0x00); 
 			deviceAccl.writeRegister(0x22, (byte) 0x00); 
