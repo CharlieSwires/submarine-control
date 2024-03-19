7bd89d7e74ba7563b32586883554fd4b1fa4db97
unknown <charlieswires@gmail.com>
2024-03-19 12:01:47 +0000

    potential fix

--- runner.sh
+++ runner.sh
@@ -4,6 +4,6 @@ echo "===================================================running================
 #export GST_DEBUG=3
 sudo pkill pigpiod
 sudo raspi-config nonint do_i2c 0
-#~/submarine-control/libcam.sh &
+~/submarine-control/libcam.sh &
 sudo java -jar ~/submarine-control/target/submarine-control.jar &
 
--- runnerHD.sh
+++ runnerHD.sh
@@ -4,6 +4,6 @@ echo "===================================================runningHD==============
 #export GST_DEBUG=3
 sudo pkill pigpiod
 sudo raspi-config nonint do_i2c 0
-#~/submarine-control/libcamHD.sh &
+~/submarine-control/libcamHD.sh &
 sudo java -jar ~/submarine-control/target/submarine-control.jar &
 
