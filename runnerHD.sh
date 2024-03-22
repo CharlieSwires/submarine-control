e20d2f63f1a3c795655c4be500e41f99d801eaa4
unknown <charlieswires@gmail.com>
2024-03-22 18:16:19 +0000

    sudo -E java -jar ~/submarine-control/target/submarine-control.jar &

--- runnerHD.sh
+++ runnerHD.sh
@@ -2,9 +2,8 @@
 #!/bin/bash
 echo "===================================================runningHD=========================================="
 #export GST_DEBUG=3
-export WATCHDOG_ENABLED=true
 sudo pkill pigpiod
 sudo raspi-config nonint do_i2c 0
 ~/submarine-control/libcamHD.sh &
-sudo java -jar ~/submarine-control/target/submarine-control.jar &
+sudo -E java -jar ~/submarine-control/target/submarine-control.jar &
 
