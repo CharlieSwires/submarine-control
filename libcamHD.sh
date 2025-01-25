51e226a57a221d798f929fa7e4262f32e0145c48
unknown <charlieswires@gmail.com>
2025-01-25 19:14:56 +0000

    h264_omx not h264 hw acceleration

--- libcamHD.sh
+++ libcamHD.sh
@@ -15,4 +15,4 @@ HOST_IP=$(hostname -I | cut -d' ' -f1)
 # Export the HOST_IP variable if needed by other scripts or applications
 export HOST_IP
 
-libcamera-vid -t 0 --width 1920 --height 1080 --codec yuv420 --framerate 25 --inline --listen -o - --verbose=0| cvlc -vvv stream:///dev/stdin --sout "#transcode{vcodec=h264,vb=2000,scale=Auto,acodec=none}:rtp{sdp=rtsp://${HOST_IP}:8554/}" --demux=rawvideo --rawvid-fps=25 --rawvid-width=1920 --rawvid-height=1080 --rawvid-chroma=I420
+libcamera-vid -t 0 --width 1920 --height 1080 --codec yuv420 --framerate 25 --inline --listen -o - --verbose=0| cvlc -vvv stream:///dev/stdin --sout "#transcode{vcodec=h264_omx,vb=2000,scale=Auto,acodec=none}:rtp{sdp=rtsp://${HOST_IP}:8554/}" --demux=rawvideo --rawvid-fps=25 --rawvid-width=1920 --rawvid-height=1080 --rawvid-chroma=I420
