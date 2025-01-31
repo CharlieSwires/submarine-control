a86f12555401faedf3833e609e8d694e41b98f9b
unknown <charlieswires@gmail.com>
2025-01-31 19:55:21 +0000

    try lower res 1280x760x10

--- libcam.sh
+++ libcam.sh
@@ -16,4 +16,4 @@ HOST_IP=$(hostname -I | cut -d' ' -f1)
 export HOST_IP
 
 # Start libcamera-vid and pipe its output directly to the GStreamer pipeline
-libcamera-vid -t 0 --width 1350 --height 760 --codec yuv420 --framerate 10 --inline --listen -o - --verbose=0 | cvlc -vvv stream:///dev/stdin --sout "#transcode{vcodec=h264,vb=1000,scale=Auto,acodec=none}:rtp{sdp=rtsp://${HOST_IP}:8554/}" --demux=rawvideo --rawvid-fps=10 --rawvid-width=640 --rawvid-height=480 --rawvid-chroma=I420
+libcamera-vid -t 0 --width 1280 --height 760 --codec yuv420 --framerate 10 --inline --listen -o - --verbose=0 | cvlc -vvv stream:///dev/stdin --sout "#transcode{vcodec=h264,vb=1000,scale=Auto,acodec=none}:rtp{sdp=rtsp://${HOST_IP}:8554/}" --demux=rawvideo --rawvid-fps=10 --rawvid-width=1280 --rawvid-height=760 --rawvid-chroma=I420
