1065285381006ce3ce78244e512f9089ee555e3f
unknown <charlieswires@gmail.com>
2025-02-27 14:57:02 +0000

    sudo ifconfig wlan0 down instead of sudo rfkill block 0

--- runnerHD.sh
+++ runnerHD.sh
@@ -4,7 +4,7 @@ echo "===================================================runningHD==============
 #export GST_DEBUG=3
 sudo pkill pigpiod
 sudo raspi-config nonint do_i2c 0
-sudo rfkill block 0
+sudo ifconfig wlan0 down
 sudo export WATCHDOG_ENABLED="true"
 ~/submarine-control/libcamHD.sh &
 sudo -E java -jar ~/submarine-control/target/submarine-control.jar &
