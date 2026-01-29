cb8fc99c2c8a66f0c8a665b7d523977ec84192a0
Charles Swires <charlieswires@gmail.com>
2026-01-29 22:27:25 +0000

    USB camera interface added

--- runner.sh
+++ runner.sh
@@ -6,6 +6,7 @@ sudo pkill pigpiod
 sudo raspi-config nonint do_i2c 0
 sudo ifconfig wlan0 down
 sudo export WATCHDOG_ENABLED="true"
-~/submarine-control/libcam.sh &
+#~/submarine-control/libcam.sh &
+WIDTH=640 HEIGHT=480 FPS=20 ~/usb_mjpeg_rtsp.sh&
 sudo -E java -jar ~/submarine-control/target/submarine-control.jar &
 
