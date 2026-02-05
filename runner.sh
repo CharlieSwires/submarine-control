b55be73d07c89fc6d0b5079479552b2db23a5db8
Charles Swires <charlieswires@gmail.com>
2026-02-05 20:07:36 +0000

    incorrect path in runner.sh and reduced the camera to 10FPS

--- runner.sh
+++ runner.sh
@@ -7,6 +7,6 @@ sudo raspi-config nonint do_i2c 0
 sudo ifconfig wlan0 down
 sudo export WATCHDOG_ENABLED="true"
 #~/submarine-control/libcam.sh &
-WIDTH=640 HEIGHT=480 FPS=20 ~/usb_mjpeg_rtsp.sh&
+WIDTH=640 HEIGHT=480 FPS=10 ~/submarine-control/usb_mjpeg_rtsp.sh&
 sudo -E java -jar ~/submarine-control/target/submarine-control.jar &
 
